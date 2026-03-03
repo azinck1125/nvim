e4aaa84a3021b4e7e82866555fc47e667b5237aa
azinck1125 <aaronzinck@gmail.com>
2026-03-03 09:40:40 -0700

    add scrolloff EOF

--- init.lua
+++ init.lua
@@ -79,7 +79,7 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 15
+vim.o.scrolloff = 10
 
 vim.o.confirm = true
 
