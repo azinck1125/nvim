b2ed31310c0ef45be9ec64e7799ea7aa08657e1c
azinck1125 <aaronzinck@gmail.com>
2026-03-26 07:53:00 -0600

    added harpoon entry options and tightened whichkey display

--- lua/ace/plugins/harpoon.lua
+++ lua/ace/plugins/harpoon.lua
@@ -16,11 +16,26 @@ return {
     vim.keymap.set('n', '<leader>j', function() harpoon.ui:toggle_quick_menu(list) end, { desc = 'Harpoon: Menu' })
 
     -- jump by index
-    vim.keymap.set('n', '<leader>1', function() list:select(1) end, { desc = 'Harpoon: (file 1)' })
-    vim.keymap.set('n', '<leader>2', function() list:select(2) end, { desc = 'Harpoon: (file 2)' })
-    vim.keymap.set('n', '<leader>3', function() list:select(3) end, { desc = 'Harpoon: (file 3)' })
-    vim.keymap.set('n', '<leader>4', function() list:select(4) end, { desc = 'Harpoon: (file 4)' })
-    vim.keymap.set('n', '<leader>5', function() list:select(5) end, { desc = 'Harpoon: (file 5)' })
+    vim.keymap.set('n', '<leader>1', function() list:select(1) end, { desc = 'Harpoon: (file 1-9)' })
+    vim.keymap.set('n', '<leader>2', function() list:select(2) end)
+    vim.keymap.set('n', '<leader>3', function() list:select(3) end)
+    vim.keymap.set('n', '<leader>4', function() list:select(4) end)
+    vim.keymap.set('n', '<leader>5', function() list:select(5) end)
+    vim.keymap.set('n', '<leader>6', function() list:select(6) end)
+    vim.keymap.set('n', '<leader>7', function() list:select(7) end)
+    vim.keymap.set('n', '<leader>8', function() list:select(8) end)
+    vim.keymap.set('n', '<leader>9', function() list:select(9) end)
+
+    require('which-key').add {
+      { '<leader>2', hidden = true },
+      { '<leader>3', hidden = true },
+      { '<leader>4', hidden = true },
+      { '<leader>5', hidden = true },
+      { '<leader>6', hidden = true },
+      { '<leader>7', hidden = true },
+      { '<leader>8', hidden = true },
+      { '<leader>9', hidden = true },
+    }
 
     -- move prev/next index
     vim.keymap.set('n', '<leader>h', function() list:prev() end, { desc = 'Harpoon prev' })
